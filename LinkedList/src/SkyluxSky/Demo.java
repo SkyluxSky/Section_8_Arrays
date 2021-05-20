dd3dfe236df076ef06f7fdce4492d530da498688
SkyluxSky <skyluxsky@gmail.com>
2021-05-19 20:02:57 -0400

    Linked List Example Finished

--- LinkedList/src/SkyluxSky/Demo.java
+++ LinkedList/src/SkyluxSky/Demo.java
@@ -3,21 +3,23 @@ package SkyluxSky;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Demo {
 
     //psvm is a shortcut for creating a new main method.
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<String>();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
-        placesToVisit.add("Canberra");
-        placesToVisit.add("Adelaide");
-        placesToVisit.add("Darwin");
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit,"Melbourne");
+        addInOrder(placesToVisit,"Brisbane");
+        addInOrder(placesToVisit,"Perth");
+        addInOrder(placesToVisit,"Canberra");
+        addInOrder(placesToVisit,"Adelaide");
+        addInOrder(placesToVisit,"Darwin");
 
         printList(placesToVisit);
+        addInOrder(placesToVisit, "Sydney");
 
         //Alice Springs will now point to Melbourne.
         placesToVisit.add(1,"Alice Springs");
@@ -26,6 +28,8 @@ public class Demo {
         //Dereferences Perth - Brisbane links to Canberra
         placesToVisit.remove(4);
         printList(placesToVisit);
+
+        visit(placesToVisit);
 
     }
 
@@ -39,12 +43,13 @@ public class Demo {
         System.out.println("=============================");
     }
 
+    //Puts Strings in ABC order
     private static boolean addInOrder(LinkedList<String> linkedList, String newCity){
         //Iterator that is useful for LinkedLists and Lists Specifically.
         ListIterator<String> stringListIterator = linkedList.listIterator();
 
         while (stringListIterator.hasNext()){
-            //gives us a value of 0 or 1
+            //gives us a value of 0 or 1 - Goes to first record
             int comparison = stringListIterator.next().compareTo(newCity);
 
             if (comparison == 0){
@@ -61,8 +66,89 @@ public class Demo {
             } else if (comparison < 0){
                 //Move on to next city.
             }
+        }
 
-            stringListIterator.add(newCity);
+        stringListIterator.add(newCity);
+        return true;
+    }
+
+
+
+    private static void visit(LinkedList cities){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if (cities.isEmpty()){
+            System.out.println("No cities in the itenerary");
+        } else {
+            System.out.println("Now visiting " + listIterator.next());
+            printMenu();
+        }
+
+        while (!quit){
+            int action = scanner.nextInt();
+            scanner.nextLine();//Buffer
+
+            switch (action){
+                case 0:
+                    System.out.println("Holiday (Vacation) over");
+                    quit = true;
+                    break;
+
+                case 1:
+                    //prevents repeating outputs.
+                    if (!goingForward){
+                        if (listIterator.hasNext()){
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+
+                    if (listIterator.hasNext()){
+                        System.out.println("Now visiting " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of the list");
+                        goingForward = false;//only direction is backwards
+                    }
+                    break;
+
+                case 2:
+                    //prevents repeating outputs.
+                    if (goingForward){
+                        if (listIterator.hasPrevious()){
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+
+                    if (listIterator.hasPrevious()){
+                        System.out.println("Now visiting " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the list");
+                        goingForward = true;//only direction is forward
+                    }
+                    break;
+
+                case 3:
+                    printMenu();
+                    break;
+                    
+                default:
+                    System.out.println("Please enter valid value");
+            }
         }
     }
+
+    private static void printMenu(){
+        System.out.println("Available Actions:\npress");
+        System.out.println("0 - to quit\n"+
+                "1 - go to next city\n"+
+                "2 - go to previous city\n"+
+                "3 - print menu options");
+    }
+
+
+
 }
