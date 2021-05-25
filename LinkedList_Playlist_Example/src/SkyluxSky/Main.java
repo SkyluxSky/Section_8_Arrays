3a25d92511e20917b4d14d8910665bea67253924
SkyluxSky <skyluxsky@gmail.com>
2021-05-25 14:57:45 -0400

    LinkedList Playlist Project Part 2 Finished

--- LinkedList_Playlist_Example/src/SkyluxSky/Main.java
+++ LinkedList_Playlist_Example/src/SkyluxSky/Main.java
@@ -16,9 +16,135 @@ package SkyluxSky;
 // As an optional extra, provide an option to remove the current song from the playlist
 // (hint: listiterator.remove())
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Scanner;
+
 public class Main {
 
+    //Creates New ArrayList of Multiple Albums
+    private static ArrayList<Album> albums = new ArrayList<Album>();
+
     public static void main(String[] args) {
-	// write your code here
+
+	    Album album = new Album("Kid A", "Radiohead");
+	    album.addSong("Everything In Its Right Place", 4.10);
+        album.addSong("Kid A", 4.40);
+        album.addSong("National Anthem", 4.20);
+        album.addSong("How To Disappear Completely", 4.00);
+        album.addSong("Treefingers", 4.10);
+        album.addSong("Optimistic", 4.30);
+        album.addSong("In Limbo", 4.50);
+        albums.add(album);
+
+        //Reuse same variable, but initiate a new album with new
+        //Frees up memory.
+        album = new Album("In An Aeroplane Over The Sea", "Neutral Milk Hotel");
+        album.addSong("King Of Carrot Flowers", 3.10);
+        album.addSong("Holland 1945", 4.40);
+        album.addSong("Communist Daughter", 2.20);
+        album.addSong("Oh Comely", 8.00);
+        album.addSong("Ghost", 3.10);
+        album.addSong("Two Headed Boy", 4.30);
+        album.addSong("In An Aeroplane Over The Sea", 4.50);
+        //adds new album to the arraylist
+        albums.add(album);
+
+
+        LinkedList<Song> playList = new LinkedList<Song>();
+        //add songs to playlist through name method.
+        albums.get(0).addToPlaylist("Kid A", playList);
+        albums.get(0).addToPlaylist("National Anthem", playList);
+        albums.get(0).addToPlaylist("In Limbo", playList);
+        albums.get(0).addToPlaylist("Man Of The World", playList); //Does Not Exist
+        //add songs to playlist through track number method.
+        albums.get(0).addToPlaylist(5, playList);
+        albums.get(1).addToPlaylist(1, playList);
+        albums.get(1).addToPlaylist(3, playList);
+        albums.get(1).addToPlaylist(5, playList);
+        albums.get(1).addToPlaylist(25, playList); //Does Not Exist
+
+        play(playList);
+
+    }
+
+
+    //Plays song
+    private static void play(LinkedList<Song> playList){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean forward = true;
+
+        ListIterator<Song> listIterator = playList.listIterator();
+
+        if (playList.size() == 0){
+            System.out.println("No songs in playlist...");
+        } else {
+            //.toString() prints out object to a readable String...
+            //Without a method like this Java will print the package name and memory address of the index of that array.
+            System.out.println("Now Playing " + listIterator.next().toString());
+        }
+
+        while (!quit){
+            int action = scanner.nextInt();
+            scanner.nextLine();//Buffer
+
+            switch(action) {
+                case 0:
+                    System.out.println("Playlist Complete");
+                    quit = true;
+                    break;
+
+                case 1:
+                    //Checks if we can go forward in playlist... Prevents duplicate output...
+                    if (!forward){
+                        if (listIterator.hasNext()){
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+
+                    //Prints Next Item...
+                    if (listIterator.hasNext()){
+                        System.out.println("Now playing " + listIterator.next().toString());
+                    } else {
+                        System.out.println("We have reached the end of the playlist");
+                        forward = false;
+                    }
+                    break;
+
+                case 2:
+
+                    if (forward){
+                        if (listIterator.hasPrevious()){
+                            listIterator.previous();
+                        }
+                        forward = false;
+                    }
+
+                    //Prints Next Item...
+                    if (listIterator.hasPrevious()){
+                        System.out.println("Now playing " + listIterator.previous().toString());
+                    } else {
+                        System.out.println("We are at the start of the playlist");
+                        forward = true;
+                    }
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    //printList(playList);
+                    break;
+                case 5:
+                    //printMenu();
+                    break;
+                default:
+                    System.out.println("Please Enter Valid Action:");
+                    break;
+            }
+        }
+
     }
 }
